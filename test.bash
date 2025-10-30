00d0362e8b7b7e52a688547ce61def4c5deaa323
Sei <busutobui@gmail.com>
2025-10-30 12:21:12 +0900

    test.bash

--- test.bash
+++ test.bash
@@ -1,13 +1,16 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2025 Seiya Ohata 　　　　　
+# SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
-	echo ${1}行目が違うよ  #$1はngの1番目の引数
+	echo ${1}行目が違うよ
 	res=1
 }
 
 res=0
-a=山田
-[ "$a" = 上田 ] || ng "$LINENO"
-[ "$a" = 山田 ] || ng "$LINENO"
 
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng "$LINENO"
+
+[ "${res}" = 0 ] && echo OK
 exit $res
